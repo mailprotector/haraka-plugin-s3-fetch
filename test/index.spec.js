e1ce74383b16c4d94c47eaac48706b7855ee141c
dallaslonesmailprotector <dallas.lones@mailprotector.com>
2021-10-15 09:35:00 -0400

    full test coverage

--- test/index.spec.js
+++ test/index.spec.js
@@ -50,7 +50,7 @@ describe('load_config', () => {
 });
 
 describe('load_s3_fetch', () => {
-  test('load_s3_fetch success without cfg credentials', (testComplete) => {
+  test('load_s3_fetch success WITHOUT cfg credentials', (testComplete) => {
     const getObjectMock = jest.fn((params, cb) => {
       cb(null, { Body: 'dater-potater' });
     });
@@ -91,6 +91,139 @@ describe('load_s3_fetch', () => {
       expect(fsMock.writeFileSync.mock.calls[0][1]).toEqual('dater-potater');
       expect(fsMock.writeFileSync.mock.calls[1][0]).toEqual('path2');
       expect(fsMock.writeFileSync.mock.calls[1][1]).toEqual('dater-potater');
+
+      testComplete();
+    };
+
+    class HarakaMock  {
+      constructor() {
+        this.cfg = {
+          files: [
+            { bucket: 'bucket1', key: 'key1', path: 'path1' },
+            { bucket: 'bucket2', key: 'key2', path: 'path2' }]
+        };
+      };
+    };
+
+    testFunc = new HarakaMock();
+    testFunc.load_s3_fetch = load_s3_fetch_test(awsMock, fsMock);
+
+    testFunc.load_s3_fetch(next, connection);
+  });
+
+  test('load_s3_fetch success WITH cfg credentials', (testComplete) => {
+    const getObjectMock = jest.fn((params, cb) => {
+      cb(null, { Body: 'dater-potater' });
+    });
+
+    class S3Mock {
+      constructor() {
+        this.getObject = getObjectMock;
+      }
+    }
+
+    const awsMock = {
+      S3: S3Mock,
+      config: {
+        update: jest.fn(() => {})
+      }
+    }
+
+    const fsMock = {
+      writeFileSync: jest.fn(() => {})
+    }
+
+    const connection = {
+      loginfo: jest.fn(msg => {
+        // console.log(msg);
+      }),
+      logdebug: jest.fn(msg => {
+        // console.log(msg);
+      })
+    };
+
+    const next = () => {
+      expect(awsMock.config.update.mock.calls[0][0]).toEqual('creds')
+
+      expect(getObjectMock.mock.calls[0][0]).toEqual(
+        { Bucket: 'bucket1', Key: 'key1' }
+      );
+      expect(getObjectMock.mock.calls[1][0]).toEqual(
+        { Bucket: 'bucket2', Key: 'key2' }
+      );
+      expect(getObjectMock.mock.calls[2]).toEqual(undefined);
+
+      expect(fsMock.writeFileSync.mock.calls[0][0]).toEqual('path1');
+      expect(fsMock.writeFileSync.mock.calls[0][1]).toEqual('dater-potater');
+      expect(fsMock.writeFileSync.mock.calls[1][0]).toEqual('path2');
+      expect(fsMock.writeFileSync.mock.calls[1][1]).toEqual('dater-potater');
+
+      testComplete();
+    };
+
+    class HarakaMock  {
+      constructor() {
+        this.cfg = {
+          files: [
+            { bucket: 'bucket1', key: 'key1', path: 'path1' },
+            { bucket: 'bucket2', key: 'key2', path: 'path2' }
+          ],
+          credentials: 'creds'
+        };
+      };
+    };
+
+    testFunc = new HarakaMock();
+    testFunc.load_s3_fetch = load_s3_fetch_test(awsMock, fsMock);
+
+    testFunc.load_s3_fetch(next, connection);
+  });
+
+  test('load_s3_fetch success WITHOUT cfg credentials AWS error', (testComplete) => {
+    const getObjectMock = jest.fn((params, cb) => {
+      cb('PHAILURE', { Body: 'dater-potater' });
+    });
+
+    class S3Mock {
+      constructor() {
+        this.getObject = getObjectMock;
+      }
+    }
+
+    const awsMock = {
+      S3: S3Mock
+    }
+
+    const fsMock = {
+      writeFileSync: jest.fn(() => {})
+    }
+
+    const connection = {
+      loginfo: jest.fn(msg => {
+        // console.log(msg);
+      }),
+      logdebug: jest.fn(msg => {
+        // console.log(msg);
+      }),
+      logerror: jest.fn(msg => {
+        // console.log(msg);
+      })
+    };
+
+    const next = () => {
+      expect(getObjectMock.mock.calls[0][0]).toEqual(
+        { Bucket: 'bucket1', Key: 'key1' }
+      );
+      expect(getObjectMock.mock.calls[1][0]).toEqual(
+        { Bucket: 'bucket2', Key: 'key2' }
+      );
+      expect(getObjectMock.mock.calls[2]).toEqual(undefined);
+
+      expect(connection.logerror.mock.calls[0][0]).toEqual('PHAILURE')
+      expect(connection.logerror.mock.calls[1][0]).toEqual('PHAILURE')
+
+      expect(fsMock.writeFileSync.mock.calls[0]).toEqual(undefined);
+      expect(fsMock.writeFileSync.mock.calls[1]).toEqual(undefined);
 
       testComplete();
     };
